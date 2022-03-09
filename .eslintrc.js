49b05c08d1d1e2fa01a3fab282225200d1fe9c04
Ahmad Madany <ahmad.madany.01@gmail.com>
2022-03-09 11:02:30 +0200

    Add dev dependencies and config files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['warn', 'tab'],
+		'no-tabs': 'off',
+
+		semi: ['error', 'never'],
+		'no-unexpected-multiline': 'error',
+		'semi-style': ['error', 'first'],
+		'no-extra-semi': 'error',
+
+		'comma-dangle': ['warn', 'always-multiline'],
+		'arrow-parens': ['warn', 'as-needed'],
+
+		'no-console': 'off',
+		'import/prefer-default-export': 'off',
+		'max-classes-per-file': 'off',
+		'no-plusplus': 'off',
+		'no-minusminus': 'off',
+		'max-len': ['error', 119],
+		'no-param-reassign': 'warn',
+	},
+}
